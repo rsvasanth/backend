d992a52e0cb7cd3e0e20a2b7879f89bd5676f3dd
rsvasanth <rsvasanth@gmail.com>
2018-01-29 17:03:00 +0530

    Signed-off-by: rsvasanth <rsvasanth@gmail.com>

--- client/app/modules/products/config/products.ctrl.js
+++ client/app/modules/products/config/products.ctrl.js
@@ -1,16 +1,48 @@
+
 (function () {
   'use strict';
-  /**
-   * @ngdoc function
-   * @name com.module.products.controller:productCtrl
-   * @description Dashboard
-   * @requires $scope
-   * @requires $rootScope
-   **/
   angular
     .module('com.module.products')
-    .controller('productCtrl', function ($scope, $rootScope) {
+    .controller('productCtrl', function ($scope, Product,Category,$state, $log) {
 
-    });
+
+      $scope.action = 'Add';
+      $scope.category = [];
+      $scope.selectedCategory;
+      $scope.product = {};
+      $scope.isDisabled = false;
+
+      Category
+        .find()
+        .$promise
+        .then(function(categories) {
+          $scope.category = categories;
+          $scope.selectedCategory = $scope.selectedCategory || categories[0];
+        });
+
+      $scope.submitForm = function() {
+        Product
+          .create({
+            name: $scope.product.name,
+            description: $scope.product.description,
+            ref_no: $scope.product.ref_no,
+            supplier_ref_no: $scope.product.supplier_ref_no,
+            futures: $scope.product.futures,
+            weight: $scope.product.weight,
+          shipping_charge: $scope.product.shipping_charge,
+          categoryId: $scope.selectedCategory.id,
+            stock: $scope.product.stock,
+            price: $scope.product.price,
+          profit_percent: $scope.product.profit,
+            supplier: $scope.product.supplier,
+            allow_order: $scope.product.allow
+          })
+          .$promise
+          .then(function() {
+            $state.go('^.list');
+          });
+      };
+
+    })
 
 })();
